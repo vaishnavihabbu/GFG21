5d801b3b6e1b0e52f73d99da46b1c525a9529c9e
Vaishnavi Habbu <vaishnavihabbu10102003@gmail.com>
2024-09-12 23:13:17 +0530

    Day 2 code uploaded

---
+++ day_2.java
@@ -0,0 +1,29 @@
+
+
+class Node{
+    int data;
+    Node next;
+}
+
+public class day_2 {
+    int getMiddle(Node head) {
+        // Your code here.
+        if(head.next==null){
+            return head.data;
+        }
+        
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast=fast.next.next;
+        }
+        
+        if(fast.next==null){//odd
+            return slow.data;
+        }else{
+            return slow.next.data;
+        }
+    }
+}
